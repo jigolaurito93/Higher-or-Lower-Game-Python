155b1643871bbfa13cb00f9e404787f517bf143b
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-26 00:53:23 -0500

    try

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from art import logo, vs
 from game_data import data
 
+# Create a flag to indicate whether game is running or not
+game_over = False
+
+# Create a while loop until game is finished
+while not game_over:
 # Print Logo
+    print(logo)
+    input('Hello')
+    game_over = True
 # If has score, print score here
 # Show comparison A
 # Print Vs
