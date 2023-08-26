3f354d525d3e2b9ac59bf8ef1b85ed3762114a38
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-26 02:03:59 -0500

    finished game

--- main.py
+++ main.py
@@ -2,54 +2,57 @@ from art import logo, vs
 from game_data import data
 import random
 
-# Create a generator to generate a random account
-account_a = random.choice(data)
-account_b = random.choice(data)
-
-# Create an if statement to check if accountA is similar to accountB, if yes, regenerate the account
-if account_a == account_b:
-    account_b = random.choice(data)
-
-# Create a function that takes the account as an argument and format the data
-def format_data(account):
-    account_name = account['name']
-    account_desc = account['description']
-    account_country = account['country']
-    print(f"{account_name}, a {account_desc}, from {account_country}.")
-
-format_data(account_a)
-
-# print(person["name"])
-
-# Print the message of the random profile
-def print_messageA(person):
-    pass
-
-
-
-
-
-
-
-rand_int = random.randrange(len(data))
-
-# Create a flag to indicate whether game is running or not
+# Set score to zero for starting game
+score = 0
+# Turn flag to False to loop game until game ends and flag turn to True
 game_over = False
 
-game_score = 0
-# Create a while loop until game is finished
+
+
+
+# Start game here
 while not game_over:
+    # Create a generator to generate a random account
+    account_a = random.choice(data)
+    account_b = random.choice(data)
+
+    # Create an if statement to check if accountA is similar to accountB, if yes, regenerate the account
+    if account_a == account_b:
+        account_b = random.choice(data)
+
+    # Create a function that takes the account as an argument and format the data
+    def format_data(account):
+        account_name = account['name']
+        account_desc = account['description']
+        account_country = account['country']
+        account_followers = account['follower_count']
+        return f"{account_name}, a {account_desc}, from {account_country}. {account_followers}"
+    
+
     # Print Logo
     print(logo)
-    # If has score, print score here
-    if game_score > 0:
-        print(game_score)
-    # Show comparison A
-    break
-    # Print Vs
-    # Show Comparison B
 
-    # Who has more followers(input)
+    # Compare account A
+    print(f"Compare A: {format_data(account_a)}")
+    # Print Vs Logo
+    print(vs)
+    # Compare account B
+    print(f"Against B: {format_data(account_b)}")
+
+    # Create an input to ask for the answer
+    answer = input(f"Who has more followers? Type 'A' or 'B': ").upper()
+
+    # Create an if statement to compare the number of followers and decide whether to quit the game or continue
+    if account_a["follower_count"] > account_b["follower_count"]:
+        if answer == "A":
+            score += 1
+        elif answer == "B":
+            game_over = True
+    elif account_a["follower_count"] < account_b["follower_count"]:
+        if answer == "A":
+            game_over = True
+        elif answer == "B":
+            score += 1
 
 
 
@@ -59,3 +62,5 @@ while not game_over:
 
 
 # when game is over, print logo and final score
+print(logo)
+print(f"Sorry, that's wrong. Final score: {score}")
