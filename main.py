ffcee1fb174e5b0d57948b37f6523073e1c1caaf
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-26 00:50:18 -0500

    set up comments to guide building the game

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hello world')
+from art import logo, vs
+from game_data import data
+
+# Print Logo
+# If has score, print score here
+# Show comparison A
+# Print Vs
+# Show Comparison B
+# Who has more followers(input)
+
+
+
+
+
+
+
+
+# when game is over, print logo and final score
